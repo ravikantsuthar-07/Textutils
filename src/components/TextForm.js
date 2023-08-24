4d401f77ac604af79f7ec7e94c15af09a21db5ee
ravikantsuthar-07 <ravikantsuthar4567@gmail.com>
2023-08-24 15:11:57 +0530

    In this we create word counting app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 
 export default function TextForm(props) {
-    const handlerUpClick = ()=>{
+    const handlerUpClick = () => {
         let newText = Text.toUpperCase();
         setText(newText);
-        console.log("Uppercase is clicked   " + Text);
     }
 
-    const handlerOnChange = (event)=>{
-        console.log("On Change");
+    const handlerLoClick = () => {
+        let newText = Text.toLowerCase();
+        setText(newText);
+    }
+    
+    const handlerClearClick = () => {
+        let newText = '';
+        setText(newText);
+    }
+
+    const handlerOnChange = (event) => {
         setText(event.target.value);
     }
 
@@ -18,11 +26,23 @@ export default function TextForm(props) {
 
     return (
         <>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={Text} onChange={handlerOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={Text} onChange={handlerOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handlerUpClick}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-1" onClick={handlerLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handlerClearClick}>Clear</button>
+
             </div>
-            <button className="btn btn-primary" onClick={handlerUpClick}>Convert to Uppercase</button>
+            <div className="container my-3">
+                <h1>Your Text Summary</h1>
+                <p>{Text.split(" ").length} Words and {Text.length } Charcters</p>
+                <p>{0.008 * Text.split(" ").length} Minutes</p>
+                <h2>Preveiws</h2>
+                <p>{Text}</p>
+            </div>
         </>
     )
 }
