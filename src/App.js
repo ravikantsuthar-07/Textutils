be289161bb784e2030bb5a9fca1a67cecd33c5af
ravikantsuthar-07 <ravikantsuthar4567@gmail.com>
2023-08-24 15:34:34 +0530

    dark mode and light mode work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" />
       <div className="container">
-        <TextForm  heading="Enter The Text Analyze"/>
+        {/* <TextForm  heading="Enter The Text Analyze"/> */}
+        <About />
       </div>
     </>
   );
